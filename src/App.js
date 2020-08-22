7bcf7dfffa2f0eee85fd64175e7857e8bf2afe7b
Tomas <tomas.milkintas1@gmail.com>
2020-08-22 21:12:55 +0100

    further experimenting with which direction to take

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
+import freelancer from "./freelancer.svg";
 import "./App.css";
 import Routes from "./Routes";
 import { Container } from "@material-ui/core";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import EmailIcon from "@material-ui/icons/Email";
 
 const App = () => {
     return (
         <div className="App">
-            <Container maxWidth="lg">
+            <Container maxWidth="xl">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={freelancer} className="App-logo" alt="logo" />
                     <p>Tomas Milkintas' Portfolio Site</p>
-                    <Routes />
+                    <div className="icons">
+                        <LinkedInIcon className="icons" fontSize="large" />
+                        <GitHubIcon className="icons" fontSize="large" />
+                        <EmailIcon className="icons" fontSize="large" />
+                    </div>
                 </header>
+                <Routes />
             </Container>
         </div>
     );
