e9fe982e71a7a477fca01284de0d991e3d0cf943
Tomas <tomas.milkintas1@gmail.com>
2020-08-31 20:54:33 +0100

    extra adjustments

--- src/containers/Projects/carouselSteps.js
+++ src/containers/Projects/carouselSteps.js
@@ -2,14 +2,14 @@ import images from "../../assets/images";
 
 const carouselSteps = [
     {
-        label: "To-do List App",
-        imgPath: images[2],
-        description: `After doing the "Picture MasterPiece" I wanted a more ambitious project. 
-        Everyone seems to do to-do list apps lately, but I wanted to make it bigger and better.
-        The idea was to have individual users create their own list of tasks, which are stored
-        in the database. Both the authentication and the data storage was accomplished using Firebase.`,
-        website: "https://magical-list.netlify.app/",
-        github: "https://github.com/tomasmilkintas/Todo-List-App",
+        label: "Burger Builder",
+        imgPath: images[0],
+        description: `This project was my first ever project using ReactJS. Now I can't take much
+        credit for it, as I followed a tutorial to get it working, but I think it's definitely
+        worthy of a mention as that's what got me into ReactJS and showed me the different and exciting
+        ways to utilize it.`,
+        website: "https://react-my-burger-app-660cf.firebaseapp.com/",
+        github: "https://github.com/tomasmilkintas/burger-app",
     },
     {
         label: "Picture MasterPiece",
@@ -22,14 +22,14 @@ const carouselSteps = [
         github: "https://github.com/tomasmilkintas/pictureRender",
     },
     {
-        label: "Burger Builder",
-        imgPath: images[0],
-        description: `This project was my first ever project using React. Now I can't take much
-        credit for it, as I followed a tutorial to get it working, but I think it's definitely
-        worthy of a mention as that's what got me into ReactJS and showed me the different and exciting
-        ways to utilize it.`,
-        website: "https://react-my-burger-app-660cf.firebaseapp.com/",
-        github: "https://github.com/tomasmilkintas/burger-app",
+        label: "To-do List App",
+        imgPath: images[2],
+        description: `After doing the "Picture MasterPiece" I wanted a more ambitious project. 
+        Everyone seems to do to-do list apps lately, but I wanted to make it bigger and better.
+        The idea was to have individual users create their own list of tasks, which are stored
+        in the database. Both the authentication and the data storage was accomplished using Firebase.`,
+        website: "https://magical-list.netlify.app/",
+        github: "https://github.com/tomasmilkintas/Todo-List-App",
     },
     {
         label: "Serverless Notes Storing App",
@@ -37,8 +37,8 @@ const carouselSteps = [
         description: `The Notes Storing App using both the backend and the frontend to connect it
         using the Serverless Framework. Serverless Framework works through AWS services, so got a
         chance to utilize quite a few of their services (IAM, Cognito, S3, Route53, DynamoDB, 
-        CloudWatch, API Gateway). In the picture you see card details being collected. This allows
-        to buy more space for your storage using Stripe service.`,
+        CloudWatch, API Gateway). In the picture you see card details being collected using Stripe service
+        for extra storage space.`,
         website: "https://serverless-stack-app.netlify.app/",
         github: "https://github.com/tomasmilkintas/serverless-stack-client",
     },
